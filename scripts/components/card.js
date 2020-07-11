c3023a7a8f9a6a2ce4c47bb1f77eea4b561ce7b3
Julian Aladio <jmaladio@gmail.com>
2020-07-11 11:54:35 -0300

    added styles on hotels card data

--- scripts/components/card.js
+++ scripts/components/card.js
@@ -1,7 +1,13 @@
 const Card = ({ name, photo, description, availableFrom, availableTo, country, city, price, rooms }) => {
 
-  const dateStart = new Date(availableFrom)
-  const dateEnd = new Date(availableTo)
+  // const dateStart = new Date(availableFrom);
+  // const dateEnd = new Date(availableTo);
+
+  const cityPlusCountry = `${city}, ${country}`;
+
+  const priceIcons = [1,2,3,4].map(el => {
+    return el <= price ? <i key={el} className="fas fa-dollar-sign"></i> : <i key={el} className="fas fa-dollar-sign dollar-disable"></i>
+  })
 
   return (
     <div className="card">
@@ -10,21 +16,40 @@ const Card = ({ name, photo, description, availableFrom, availableTo, country, c
         <h2 className="heading-secondary">{name}</h2>
         <p className="paragraph-card">{description}</p>
         <br />
-        <div className="data__rooms">
-          <div>
-            <i className="fas fa-bed"></i>
-          </div>
-          <div>
-            <p>{rooms} habitaciones</p>
+
+        <div className="card__data--line-1">
+          <div className="card__place">
+            <div className="card__place--icon">
+              <i className="fas fa-map-marker"></i>
+            </div>
+            <div className="card__place--text">
+              <p>{cityPlusCountry}</p>
+            </div>
           </div>
         </div>
+
+
+        <div className="card__data--line-2">
+          <div className="card__rooms">
+            <div className="card__rooms--icon">
+              <i className="fas fa-bed"></i>
+            </div>
+            <div className="card__rooms--text">
+              <p>{rooms} habitaciones</p>
+            </div>
+          </div>
+          <div className="card__price">
+          {priceIcons}
+        </div>
+        </div>
         
-        <p>From: {dateStart.toISOString()}</p>
+        
+        {/* <p>From: {dateStart.toISOString()}</p>
         <p>To: {dateEnd.toISOString()}</p>
         <p>Country: {country}</p>
         <p>City: {city}</p>
         <p>Price range ID: {price}</p>
-        <p>Rooms: {rooms}</p>
+        <p>Rooms: {rooms}</p> */}
       </div>
       <button className="card__btn">Reservar</button>
     </div>
